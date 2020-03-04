5dbbdda1e9050ea00f42c325f08bed5d23cbbb3f
jasonjiangstuy <jasonjiangny@gmail.com>
2020-03-04 11:21:17 -0500

    html encoding problem solved

--- details.py
+++ details.py
@@ -3,11 +3,20 @@
 def list(listitems):
     final = "<ul>"
     for i in listitems:
-        final.append("<li>" + str(i) + "</li>")
-    final.append("</ul>")
+        final = final +"<li>" + str(i) + "</li>"
+    final = final + "</ul>"
     return(final)
-    
-citation = [
+
+def stripquotes(list):
+    finallist = []
+    for i in list:
+        if type(i) == type(""):
+            finallist.append(i.strip("/'" + '/"'))
+        elif type(i) == type([]):
+            finallist.append(stripquotes(i))
+    return(finallist)
+
+citation = stripquotes([
     "Citations",
   "This are the citations for what used to build this website",
     [
@@ -15,10 +24,10 @@ citation = [
       ["HTML + CSS reference", "Devdocs, I used this service to learn about a few specific attributes + functions that I used to make my website pretty"],
       ["jninja reference", "I used <a href='https://jinja.palletsprojects.com/en/2.11.x/'> this website </a> to review a few jinja features that I forgot the format for", "img."
       ],
-      ["Stackoverflow", "I used stack overflow to trouble shoot and solve various problems<br> including: " + list("how to preload and cashe images - index.html(12)")]
+      ["Stackoverflow", "I used stack overflow to trouble shoot and solve various problems<br> including: " + list(["how to preload and cashe images - index.html(12)"])]
 
     ]
-]
+])
 
 projects = [
   "My Projects",
